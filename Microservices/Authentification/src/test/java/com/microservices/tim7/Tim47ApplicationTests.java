321814b77dd783a8f081127c42bec3e0a40ba73d
damjanbanjac <damjanbanjac14@gmail.com>
2020-05-16 14:15:16 +0200

    servis za autentifikaciju

---
+++ Microservices/Authentification/src/test/java/com/microservices/tim7/Tim47ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.tim7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tim47ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
