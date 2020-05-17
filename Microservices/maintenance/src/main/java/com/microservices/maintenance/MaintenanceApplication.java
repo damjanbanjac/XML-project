64b0fbaa1645ef312fd1dab1e0b469be56a9af58
laramimica <lara.mimica@gmail.com>
2020-05-17 13:51:46 +0200

    servis za odrzavanje

---
+++ Microservices/maintenance/src/main/java/com/microservices/maintenance/MaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceApplication.class, args);
+	}
+
+}
