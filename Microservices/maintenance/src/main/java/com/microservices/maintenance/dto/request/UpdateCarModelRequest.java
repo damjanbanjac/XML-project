6a2bf0b5091f08be277b1914f4d860fe1615aa78
laramimica <lara.mimica@gmail.com>
2020-05-17 15:56:19 +0200

    Napravljen entitet modela automobila i interface sa crud-om

---
+++ Microservices/maintenance/src/main/java/com/microservices/maintenance/dto/request/UpdateCarModelRequest.java
@@ -0,0 +1,10 @@
+package com.microservices.maintenance.dto.request;
+
+import lombok.Data;
+
+@Data
+public class UpdateCarModelRequest {
+
+    private String model;
+
+}
---
+++ Microservices/maintenance/src/main/java/com/microservices/maintenance/dto/response/CarModelResponse.java
@@ -0,0 +1,15 @@
+package com.microservices.maintenance.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarModelResponse {
+
+    private long id;
+
+    private String model;
+}
