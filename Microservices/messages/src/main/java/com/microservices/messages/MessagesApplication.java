a91d099e0b7a89ef398cd6b1e6039e8a1ff1e9b5
damjanbanjac <damjanbanjac14@gmail.com>
2020-05-31 21:10:58 +0200

    dodatna podesavanja u doker compose fajlu

--- Microservices/messages/src/main/java/com/microservices/messages/MessagesApplication.java
+++ Microservices/messages/src/main/java/com/microservices/messages/MessagesApplication.java
@@ -2,8 +2,10 @@ package com.microservices.messages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MessagesApplication {
 
 	public static void main(String[] args) {
