35ac0d54d7d9f3dc8933e1e7fc40f193da6952f6
damjanbanjac <damjanbanjac14@gmail.com>
2020-06-01 19:49:50 +0200

    radjeno na doavanju oglasa

---
+++ Microservices/ads/src/main/java/com/microservices/ads/controller/AdCarController.java
@@ -0,0 +1,33 @@
+package com.microservices.ads.controller;
+
+import com.microservices.ads.dto.request.AdCarRequest;
+import com.microservices.ads.dto.response.AdCarResponse;
+import com.microservices.ads.service.AdsCarService;
+import com.microservices.ads.service.IAdsCarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ads")
+public class AdCarController {
+
+
+    @Autowired
+    private AdsCarService adsCarService;
+
+
+    @PostMapping
+    public AdCarResponse addAd(@RequestBody AdCarRequest request) throws Exception{
+        return adsCarService.createAd(request);
+    }
+
+    @PutMapping("/{id}/ad")
+    public AdCarResponse updateAd(@RequestBody AdCarRequest request, @PathVariable long id) throws Exception{
+        return adsCarService.updateAd(request, id);
+    }
+
+    @DeleteMapping("/{id}/ad")
+    public void deleteAd(@PathVariable long id) throws Exception{
+        adsCarService.deleteAdCar(id);
+    }
+}
---
+++ Microservices/ads/src/main/java/com/microservices/ads/model/CarBrand.java
@@ -0,0 +1,21 @@
+package com.microservices.ads.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarBrand {
+
+    @Id
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    private Long id;
+}
