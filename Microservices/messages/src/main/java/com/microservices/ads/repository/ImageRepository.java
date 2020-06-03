16bfda4d05e4fd4b4e43ff8cf2cb9c84c9ca7f70
damjanbanjac <damjanbanjac14@gmail.com>
2020-06-03 02:12:06 +0200

    podizanje baze

--- Microservices/messages/src/main/java/com/microservices/ads/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.ads.repository;
-
-import com.microservices.ads.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-}
-
