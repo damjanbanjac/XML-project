f151ac444455c1552923ce1dcd07819ebd3c5310
laramimica <lara.mimica@gmail.com>
2020-06-03 23:15:02 +0200

    Minor changes

--- XML-front/src/components/NavBar.vue
+++ XML-front/src/components/NavBar.vue
@@ -10,13 +10,10 @@
 
     <b-navbar-nav class="ml-auto" v-if="this.$store.state.user.enabled == true">
       <b-nav-item-dropdown
-        text="Sidebar"
+        text="Pricelist"
         right
        v-if="this.$store.state.user.role.authority == 'USER'"
       >
-        <router-link to="/agentRegistration" tag="b-dropdown-item">Register agent/company</router-link>
-        <router-link to="/userList" tag="b-dropdown-item">Users</router-link>
-        <router-link to="/registrationRequests" tag="b-dropdown-item">Registration requests</router-link>
         <router-link to="/createPriceList" tag="b-dropdown-item">Add pricelist</router-link>
          <div class="dropdown-divider"></div>
         <b-dropdown-item @click="odjavaFun">Log Out</b-dropdown-item>
