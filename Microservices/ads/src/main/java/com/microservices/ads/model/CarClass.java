4224893b97a177572f36a37eb37924dba03fcb66
damjanbanjac <damjanbanjac14@gmail.com>
2020-06-05 16:29:47 +0200

    resavanja bagova kod oglasa

--- Microservices/ads/src/main/java/com/microservices/ads/model/CarClass.java
+++ Microservices/ads/src/main/java/com/microservices/ads/model/CarClass.java
@@ -6,15 +6,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "carClassAd")
 public class CarClass {
     @Id
     @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
     private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "carClass_id")
+    public List<AdCar> adsCar;
 }
