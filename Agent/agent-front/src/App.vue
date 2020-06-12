599995745da648515e4dec3e6d5adac224de8e30
laramimica <lara.mimica@gmail.com>
2020-06-12 16:57:00 +0200

    Agent front - maintenance by admin: adding and lists

--- Agent/agent-front/src/App.vue
+++ Agent/agent-front/src/App.vue
@@ -30,7 +30,7 @@ export default {
 }
 
 body {
-    background-image: url('https://scontent.fbeg2-1.fna.fbcdn.net/v/t1.15752-9/s2048x2048/100515875_881490369020792_4518760818975703040_n.jpg?_nc_cat=107&_nc_sid=b96e70&_nc_eui2=AeGnhsfbpMDeEEeIz0Yx4K5e_341XRcPUa7_fjVdFw9RrsyTVLUM5NwB3uTLNdD-vs8&_nc_ohc=JqfGBin3MQEAX_eG0NR&_nc_ht=scontent.fbeg2-1.fna&_nc_tp=7&oh=f0f305a21997a2033deb614dad0df1fe&oe=5EF2CF03');
+    background-image: url('https://scontent.fbeg2-1.fna.fbcdn.net/v/t1.15752-9/103881952_261397135081687_2774286197962572060_n.jpg?_nc_cat=104&_nc_sid=b96e70&_nc_eui2=AeG0zS1zc4uQEvkVZHt6qEFHSnw4ORzmjRRKfDg5HOaNFP7U6NSHG0qKynEQNMVwlys&_nc_ohc=BcGs03Ta13MAX9TVA32&_nc_ht=scontent.fbeg2-1.fna&oh=5a59e15199e1fe61717548ac85075cb9&oe=5F085A07');
     background-repeat: no-repeat;
 }
 </style>
--- Agent/agent-front/src/router/index.js
+++ Agent/agent-front/src/router/index.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import NewAd from '../components/Ad/NewAd.vue'
 import OpenedAd from '../components/Ad/OpenedAd.vue'
-import AgentRegistrationForm from '../components/views/Admin/AgentRegistrationForm.vue'
-import RegistrationRequestList from '../components/views/Admin/RegistrationRequestList.vue'
-import UserList from '../components/views/Admin/UserList.vue'
+
 import AdsList from '../components/Ad/AdsList.vue'
+
+import AgentRegistrationForm from '../components/views/Admin/AgentRegistrationForm.vue'
+
+import AddCarBrand from '../components/views/Admin/add/AddCarBrand.vue'
+import AddCarClass from '../components/views/Admin/add/AddCarClass.vue'
+import AddCarModel from '../components/views/Admin/add/AddCarModel.vue'
+import AddTypeOfFuel from '../components/views/Admin/add/AddTypeOfFuel.vue'
+import AddTypeOfGearshift from '../components/views/Admin/add/AddTypeOfGearshift.vue'
+
+import RegistrationRequestList from '../components/views/Admin/lists/RegistrationRequestList.vue'
+import UserList from '../components/views/Admin/lists/UserList.vue'
+import CarBrandList from '../components/views/Admin/lists/CarBrandList.vue'
+import CarClassList from '../components/views/Admin/lists/CarClassList.vue'
+import CarModelList from '../components/views/Admin/lists/CarModelList.vue'
+import FuelTypeList from '../components/views/Admin/lists/FuelTypeList.vue'
+import GearshiftTypeList from '../components/views/Admin/lists/GearshiftTypeList.vue'
 
 Vue.use(VueRouter)
 
@@ -40,6 +55,57 @@ const routes = [
         name: 'registration-requests',
         component: RegistrationRequestList
       },
+      {
+        path: '/carBrandList',
+        name: 'car-brand-list',
+        component: CarBrandList
+      },
+      {
+        path: '/addCarBrand',
+        name: 'add-car-brand',
+        component: AddCarBrand
+      },
+      {
+        path: '/addCarClass',
+        name: 'add-car-class',
+        component: AddCarClass
+      },
+      {
+        path: '/addCarModel',
+        name: 'add-car-model',
+        component: AddCarModel
+      },
+      {
+        path: '/addTypeOfFuel',
+        name: 'add-type-of-fuel',
+        component: AddTypeOfFuel
+      },
+      {
+        path: '/addTypeOfGearshift',
+        name: 'add-type-of-gearshift',
+        component: AddTypeOfGearshift
+      },
+      {
+        path: '/carClassList',
+        name: 'car-class-list',
+        component: CarClassList
+      },
+      {
+        path: '/carModelList',
+        name: 'car-model-list',
+        component: CarModelList
+      },
+      {
+        path: '/fuelTypeList',
+        name: 'fuel-type-list',
+        component: FuelTypeList
+      },
+      {
+        path: '/gearshiftTypeList',
+        name: 'gearshift-type-list',
+        component: GearshiftTypeList
+      },
+
 
 ]
 
