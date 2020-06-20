e38853629c2e2642ca9066b989c705e505d39cc6
laramimica <lara.mimica@gmail.com>
2020-06-20 21:29:03 +0200

    Commenting by agent

--- Agent/agent-front/src/components/Agent/Ad/CommentList.vue
+++ Agent/agent-front/src/components/Agent/Ad/CommentList.vue
@@ -8,23 +8,46 @@
           <h3 class="deep-grey-text mt-3 mb-4 pb-1 mx-5">Comments</h3>
         </div>
       </div>
-      <!--Header-->
 
+      <!-- Table -->
+      <b-container>
+              <div>
+          <b-table class="mt-2 mb-2" striped hover :items="comments" select-mode="single" :fields="fields" caption-top >
+          </b-table>
+        </div>
+      </b-container>
+    </div>
 
+    <!-- Comment -->
+    <div class="col">
 
+        <b-container v-if="success">
+          <b-alert show variant="success" class="d-flex justify-content-center">{{successmessages}}</b-alert>
+        </b-container>
 
-<b-container>
-         <div>
-    <b-table class="mt-2 mb-2" striped hover :items="comments" select-mode="single" :fields="fields" caption-top >
-    </b-table>
-  </div>
-</b-container>
-  
+        <div class="card" style="width: 96%;">
+          <!--Header-->
+          <div class="header pt-3 grey lighten-2">
+            <div class="row d-flex justify-content-start">
+              <h3 class="deep-grey-text mt-3 mb-4 pb-1 mx-5" 
+              style="font-size: 3rem;
+              font-weight: 300;
+              line-height: 1.2;
+              margin-top: -12%;">Send feedback</h3>
+            </div>
+          </div>
 
-
-    
-
-      
+          <div class="card-body mx-4 mt-4">
+            <label for="Form-text">Comment</label>
+            <input type="text" id="Form-text" class="form-control" v-model="comm" />
+            <br/>
+            <button
+            type="button"
+                  class="btn btn-info btn-block z-depth-2"
+                  @click="comment()">Comment
+            </button> 
+          </div>
+          </div>
     </div>
     
   </div>
@@ -37,10 +60,33 @@ export default {
     return {
       comments: [],
       fields: [
-        {
-          key: 'comment'},
-        ]
+        {key: 'comment'},
+      ],
+      comm: '',
+      error: '',
+      success: false,
+      successmessages: "",
     };
+  },
+
+  methods: {
+    comment() {
+      const body = {
+        comment: this.comm,
+        adCarId: this.$route.params.id
+      }
+      axios
+        .post("/comments", body)
+        .then(() => {
+          this.comm = "";
+          this.success = true;
+          this.successmessage = "You have successfully commented this car ad.";
+        })
+        .catch(error => {
+          console.log(error);
+          this.error = true;
+        });
+    }
   },
    
   mounted() {
