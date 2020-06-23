c4fa76650d520ac69e1827726f263cf6483c23d4
damjanbanjac <damjanbanjac14@gmail.com>
2020-06-23 15:21:58 +0200

    izmena fajla

--- Microservices/authentication/src/main/resources/data.sql
+++ Microservices/authentication/src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO `services`.`authority` (`id`, `name`) VALUES ('1', 'USER');
 INSERT INTO `services`.`authority` (`id`, `name`) VALUES ('2', 'ADMIN');
 INSERT INTO `services`.`authority` (`id`, `name`) VALUES ('3', 'AGENT');
 
-INSERT INTO `services`.`admin_auth` (`id`, `active`, `blocked`, `country`, `email`, `name`, `password`, `permission_blocked`, `phone_number`) VALUES ('1', b'true', b'false', b'serbia', b'admin@admin.com', b'admin', b'123', b'false', b'381');
+INSERT INTO `services`.`admin_auth` (`id`, `active`, `blocked`, `country`, `email`, `name`, `password`, `permission_blocked`, `phone_number`) VALUES ('1', true, false, 'serbia', '$2a$10$mzFkdgfA8T.UiOJLXLIe7.tcEgaoUyXDpDuBlsAZQrqQoDRyMw6Ae', 'admin', '123', false, '381');
 
-INSERT INTO `services`.`agent_auth` (`id`, `active`, `blocked`, `country`, `email`, `name`, `password`, `permission_blocked`, `phone_number`) VALUES ('1', b'true', b'false', b'serbia', b'damjanbanjac@gmail.com', b'damjan', b'123', b'false', b'333');
+INSERT INTO `services`.`agent_auth` (`id`, `active`, `blocked`, `country`, `email`, `name`, `password`, `permission_blocked`, `phone_number`) VALUES ('1', true, false, 'serbia', 'damjanbanjac@gmail.com', 'damjan', '$2a$10$mzFkdgfA8T.UiOJLXLIe7.tcEgaoUyXDpDuBlsAZQrqQoDRyMw6Ae', false, '333');
