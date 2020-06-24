3f23e7d138c9ff054b9857fb607c8e87170ee771
damjanbanjac <damjanbanjac14@gmail.com>
2020-06-24 14:18:53 +0200

    ispravljanje logovanja

---
+++ Agent/agent-app/src/main/java/com/agent/agentapp/entity/Authority.java
@@ -0,0 +1,44 @@
+package com.agent.agentapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Authority implements GrantedAuthority {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+
+    String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonIgnore
+    public String getName() {
+        return name;
+    }
+
+    @JsonIgnore
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
