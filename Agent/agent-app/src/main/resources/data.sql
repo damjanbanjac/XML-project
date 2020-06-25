1c4d810280a6995b6426b1605d2ff567b02eea41
unknown <bozidar.kljajevic07@gmail.com>
2020-06-25 13:33:32 +0200

    dodate permisije na agentskoj strani

--- Agent/agent-app/src/main/resources/data.sql
+++ Agent/agent-app/src/main/resources/data.sql
@@ -2,14 +2,45 @@
 
 INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('1', 'CREATE-AD');
 INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('2', 'UPADATE-AD');
-INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('3', 'CHECK');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('3', 'DELETE-AD');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('4', 'UN/BLOCK-USER');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('5', 'DE/ACTIVATE-USER');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('6', 'APPROVE/DENY-COMMENT');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('7', 'APPROVE/DENY-REGISTRATION');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('8', 'CRUD-CAR-COMPONENTS');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('9', 'POST-COMMENT');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('10', 'POST-GRADE');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('11', 'CRUD-IMAGE');
+INSERT INTO `agentxml`.`permission` (`id`, `name`) VALUES ('12', 'CREATE-REPORT');
 
-INSERT INTO `agentxml`.`authority` (`id`, `name`) VALUES ('1', 'USER');
+INSERT INTO `agentxml`.`authority` (`id`, `name`) VALUES ('1', 'ADMIN');
 INSERT INTO `agentxml`.`authority` (`id`, `name`) VALUES ('2', 'AGENT');
+INSERT INTO `agentxml`.`authority` (`id`, `name`) VALUES ('3', 'USER');
+INSERT INTO `agentxml`.`authority` (`id`, `name`) VALUES ('4', 'USER-SELLER');
 
 
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('1', '4');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('1', '5');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('1', '6');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('1', '7');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('1', '8');
 INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '1');
 INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '2');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '3');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '8');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '9');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '10');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '11');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('2', '12');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('3', '9');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('3', '10');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('4', '1');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('4', '2');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('4', '8');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('4', '9');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('4', '10');
+INSERT INTO `agentxml`.`authorities_permissions` (`authority_id`, `permission_id`) VALUES ('4', '11');
+
 --INSERT INTO `services`.`authority` (`id`, `name`) VALUES ('2', 'ADMIN');
 --INSERT INTO `services`.`authority` (`id`, `name`) VALUES ('3', 'AGENT');
 
