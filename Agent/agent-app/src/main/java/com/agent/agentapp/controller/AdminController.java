14bedbf31e13dcc6449f4612e64596c62fa5b3d6
damjanbanjac <damjanbanjac14@gmail.com>
2020-06-26 18:59:09 +0200

    reseni bagovi u skripti

--- Agent/agent-app/src/main/java/com/agent/agentapp/controller/AdminController.java
+++ Agent/agent-app/src/main/java/com/agent/agentapp/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.agent.agentapp.dto.request.GetIdRequest;
 import com.agent.agentapp.dto.response.CommentResponse;
 import com.agent.agentapp.dto.response.RegistrationResponse;
 import com.agent.agentapp.service.IAdminService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,40 +23,40 @@ public class AdminController {
 
     @PutMapping("/block/{id}/user")
     // @PreAuthorize("hasAuthority('UN/BLOCK-USER')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public void blockUser(@PathVariable Long id) {
         _adminService.blockUser(id);
     }
 
     @PutMapping("/unblock/{id}/user")
     // @PreAuthorize("hasAuthority('UN/BLOCK-USER')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public void unblockUser(@PathVariable Long id) {
         _adminService.unblockUser(id);
     }
 
     @PutMapping("/activate/{id}/user")
     // @PreAuthorize("hasAuthority('DE/ACTIVATE-USER')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public void activateUser(@PathVariable Long id) {
         _adminService.activateUser(id);
     }
 
     @PutMapping("/deactivate/{id}/user")
     // @PreAuthorize("hasAuthority('DE/ACTIVATE-USER')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public void deactivateUser(@PathVariable Long id) {
         _adminService.deactivateUser(id);
     }
 
     @PutMapping("/approve/{id}/comment")
     // @PreAuthorize("hasAuthority('APPROVE/DENY-COMMENT')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public CommentResponse approveComment(@PathVariable Long id) { return _adminService.approveComment(id); }
 
     @PutMapping("/deny/comment")
     // @PreAuthorize("hasAuthority('APPROVE/DENY-COMMENT')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public CommentResponse denyComment(@RequestBody DenyCommentRequest id) { return _adminService.denyComment(id); }
 
     @GetMapping("/pending-comments")
@@ -65,14 +66,14 @@ public class AdminController {
 
     @PostMapping("/approve-registration")
     // @PreAuthorize("hasAuthority('APPROVE/DENY-REGISTRATION')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public void approveRegistration(@RequestBody GetIdRequest request){
         _adminService.approveRegistration(request);
     }
 
     @PostMapping("/deny-registration")
     // @PreAuthorize("hasAuthority('APPROVE/DENY-REGISTRATION')")
-    // @PreAuthorize("hasAuthority('ADMIN')")
+     @PreAuthorize("hasAuthority('ADMIN')")
     public void denyRegistration(@RequestBody GetIdRequest request){
         _adminService.denyRegistration(request);
     }
